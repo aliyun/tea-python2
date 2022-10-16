8925f40edf447e6983e37a2f34927546eb54d3c8
nanhe <papaya.dyn@alibaba-inc.com>
2022-10-16 15:29:37 +0800

    feat: return more detail in error info

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -23,7 +23,14 @@ class TestTeaException(TestCase):
             self.assertEqual("Retry failed: test exception", e.message)
 
     def test_tea_exception(self):
-        dic = {"code": "200", "message": "message", "data": {"test": "test"}}
+        dic = {"code": "200", "message": "message", "data": {"test": "test", "statusCode": 200},
+               "description": "description",
+               "accessDeniedDetail": {
+                   'AuthAction': 'ram:ListUsers',
+                   'AuthPrincipalType': 'SubUser',
+                   'PolicyType': 'ResourceGroupLevelIdentityBassdPolicy',
+                   'NoPermissionType': 'ImplicitDeny'
+               }}
         try:
             raise TeaException(dic)
         except TeaException as e:
@@ -32,6 +39,14 @@ class TestTeaException(TestCase):
             self.assertEqual("message", e.message)
             self.assertIsNotNone(e.data)
             self.assertEqual("test", e.data.get("test"))
+            self.assertEqual(200, e.statusCode)
+            self.assertEqual("description", e.description)
+            self.assertDictEqual({
+                'AuthAction': 'ram:ListUsers',
+                'AuthPrincipalType': 'SubUser',
+                'PolicyType': 'ResourceGroupLevelIdentityBassdPolicy',
+                'NoPermissionType': 'ImplicitDeny'
+            }, e.accessDeniedDetail)
 
     def test_RequiredArgumentException(self):
         param_name = 'name'
